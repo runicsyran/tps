6ce9944eea8b38b2cc845d610c77364a20f7e78b
Michelangelo Cuccui <mickeycp2006@gmail.com>
2025-02-14 09:07:31 +0100

    aggiornamento 2025 02 14 09:07

--- connessioniWebSocket/Main.java
+++ connessioniWebSocket/Main.java
@@ -8,11 +8,18 @@ public class Main {
             HttpURLConnection cernHomeConnection = (HttpURLConnection) cernHome.openConnection(); // Apre la connessione
             InputStream in = cernHomeConnection.getInputStream(); // Apre lo stream di input (sequenza binaria di dati)
             byte [] data = new byte[1024]; // Crea un array di byte
-            in.read(data); // Legge i dati dallo stream di input e li memorizza nell'array di byte
+            try{
+                in.read(data); // Legge i dati dallo stream di input e li memorizza nell'array di byte
+            } catch (IOException e){
+                throw new RuntimeException(e);
+            }
+            for (int i=0; i<data.length; i++){
+                System.out.print(data[i] + "-"); // Stampa i dati letti
+            }
             
-        } catch (MalformedURLException e){
-            throw new RuntimeException(e);
-        } catch (IOException e){
+        } catch (MalformedURLException e){ 
+            throw new RuntimeException(e); 
+        } catch (IOException e){ 
             throw new RuntimeException(e);
         }
 
